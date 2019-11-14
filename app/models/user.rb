e0d3ca55c63705d130f43483acae7427b3fc55e7
Leo <ronald.mauritsson@gmail.com>
2019-11-15 01:29:15 +0300

    Created comment model and added associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  has_many :posts, dependent: :destroy
+  has_many :posts, dependent: destroy
+  has_many :comments
 
   validates :name, presence: true
   validates :email, presence: true
---
+++ db/migrate/20191114220711_add_post_ref_to_comments.rb
@@ -0,0 +1,4 @@
+class AddPostRefToComments < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
