0692c6b65be19861620806d8d4a1d5ee98630a84
SofiBretz <sofiabarrantes.gamboa@gmail.com>
2019-11-14 22:00:33 -0300

    foreign key fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :posts, dependent: destroy
-  has_many :comments
+  has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   validates :name, presence: true
   validates :email, presence: true
--- db/migrate/20191114220238_create_comments.rb
+++ db/migrate/20191114220238_create_comments.rb
@@ -1,8 +1,7 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.integer :post_id
-      t.integer :user_id
+
       t.string :content
 
       t.timestamps
